7b42f7f11eb5ebc4843c145bfc985ebee6aab4d9
vls <belyakov38@gmail.com>
2018-11-13 20:08:01 +0300

    started

--- commom_calc/base.py
+++ commom_calc/base.py
@@ -1,15 +1,25 @@
 people = []
 summ = []
 
-def calc(a):
-    a = []
+
+def calc_person(a):
+    #a = []
     n = int(input())
     for i in range(n):
         new_element = str(input())
         a.append(new_element)
 
-calc(people)
-calc(summ)
+
+def calc_bill(b):
+    #b = []
+    n = int(input())
+    for i in range(n):
+        new_element = int(input())
+        b.append(new_element)
+
+
+calc_person(people)
+calc_bill(summ)
 
 print(people)
 print(summ)
