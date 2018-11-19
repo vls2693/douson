2e83212cb51a7a42da9cb3899878dc92cf8f6098
vls2693 <belyakov38@gmail.com>
2018-11-19 21:56:59 +0300

    объединил 2 функции в одной, обновил отчёт, начал делать метод транзакций

--- commom_calc/base.py
+++ commom_calc/base.py
@@ -1,37 +1,58 @@
-people = ['Риваль', 'Василий', 'Геннадий', 'Егор', 'Михаил', 'Ильнар', 'Дядя Саша'] # люди
+people = ['Риваль', 'Василий', 'Геннадий', 'Егор', 'Михаил', 'Ильнар',
+          'Дядя Саша'] # люди
 summ = [1771, 638, 500, 0, 0, 0, 0] # потраченное каждым
 
+
 def amount(summ):
-    amount = 0;
+    amount = 0
     for i in range(0, len(summ)):
         amount = round(amount + summ[i], 2)
     return amount
+
 
 def average(summ, amount): # среднее арифметическое потраченного
     average = round(amount / len(summ), 2)
     return average
 
-def shortage(summ, average): # определяем кто сколько должен доплатить
-    shortage = []
-    for i in range(0, len(summ)):
-        short = round(average - summ[i], 2)
-        shortage.append(short)
-    return shortage
-
-def money(summ, average): # определяем кто сколько должен получить
-    money = []
-    for i in range(0, len(summ)):
-        inc = round(summ[i] - average, 2)
-        money.append(inc)
-    return money
 
 amount = amount(summ)
 average = average(summ, amount)
-shortage = shortage(summ, average)
-money = money(summ, average)
 
 
-def report(people, summ, amount, average, shortage, money): # отчёт
+def distributor(people, summ, average):
+    plus_people = []
+    minus_people = []
+    plus_summ = []
+    minus_summ = []
+    for i in range(0, len(summ)):
+        if summ[i] > average:
+            m = summ[i] - average
+            plus_summ.append(m)
+            plus_people.append(people[i])
+        elif summ[i] < average:
+            n = average - summ[i]
+            minus_summ.append(n)
+            minus_people.append(people[i])
+        else:
+            continue
+    return plus_people, plus_summ, minus_people, minus_summ
+
+
+def transaction(plus_people, plus_summ, minus_people, minus_summ):
+    for i in range(0, len(minus_summ)):
+        for l in range(len(plus_summ)):
+            print()
+
+
+
+plus_people, plus_summ, minus_people, minus_summ = distributor(people, summ,
+                                                               average)
+
+print(plus_people, plus_summ, minus_people, minus_summ)
+
+
+def report2(people, summ, amount, average, plus_people, plus_summ, minus_people,
+            minus_summ): # отчёт
     print('Скидывали:')
     for i in range(0, len(summ)):
         if summ[i] != 0:
@@ -39,19 +60,19 @@ def report(people, summ, amount, average, shortage, money): # отчёт
     print()
     print("Всего потрачено: " + str(amount))
     print()
-    print("С каждого по " + str(average) + "р.")
+    print("Среднее арифметическое потраченного " + str(average) + "р.")
     print()
     print("Должны скинуть: ")
-    for i in range(0, len(shortage)):
-        if shortage[i] > 0:
-            print(str(people[i]) + ' - ' + str(shortage[i]) + "р.")
+    for i in range(0, len(minus_people)):
+        print(str(minus_people[i]) + ' - ' + str(minus_summ[i]) + "р.")
     print()
     print("Должны получить: ")
-    for i in range(0, len(money)):
-        if money[i] > 0:
-            print(str(people[i]) + ' - ' + str(money[i]) + "р.")
-report(people, summ, amount, average, shortage, money)
+    for i in range(0, len(plus_people)):
+        print(str(plus_people[i]) + ' - ' + str(plus_summ[i]) + "р.")
 
+
+report2(people, summ, amount, average, plus_people, plus_summ, minus_people,
+            minus_summ)
 
 '''
 def calc_person(b):
@@ -74,3 +95,8 @@ calc_bill(summ)
 print(people)
 print(summ)
 '''
+
+d = {'Риваль': 1771, 'Василий': 638, 'Геннадий': 500, 'Егор': 0, 'Михаил': 0,
+     'Ильнар': 0, 'Дядя Саша': 0}
+print(dict.items(d))
+print(dict.keys(d))
