126b587390ba202379fff922135123e087cd5957
VLS <belyakov3@gmail.com>
2021-02-27 19:53:23 +0300

    1. заменил текст на анлийский, 2. исправил баг с вводом пустых значений, 3. сделал 10 полей, 4. сделал все поля необязательными для заполнения

--- commom_calc/average.py
+++ commom_calc/average.py
@@ -1,4 +1,7 @@
 # среднее арифметическое потраченного
 def average_counter(list_of_persons, amount):
-    average = round(amount / len(list_of_persons), 2)
-    return average
+    if len(list_of_persons) == 0:
+        print("Division by zero is impossible")
+    else:
+        average = round(amount / len(list_of_persons), 2)
+        return average
--- commom_calc/report.py
+++ commom_calc/report.py
@@ -4,9 +4,9 @@ from flask import render_template
 def report(people, common_sum, amount, average, plus_people_rep, plus_sum_rep,
            minus_people_rep, minus_sum_rep, transaction_minus,
            transaction_sum, transaction_plus):  # отчёт
-    name_list = 'Скидывали:'
+    name_list = 'Who spent money:'
     br = '<br>'
-    back_link = '<a href="/">Вернуться назад</a>'
+    back_link = '<a href="/">Come back to the previous page</a>'
     with open("../flask/templates/file.html", "w", encoding="utf-8") as file:
         file.write(name_list)
         file.write(br)
@@ -16,27 +16,27 @@ def report(people, common_sum, amount, average, plus_people_rep, plus_sum_rep,
             file.write(names)
             file.write(br)
         file.write(br)
-        file.write('Всего потрачено: {}р.'.format(amount))
+        file.write('Common spent sum: {}р.'.format(amount))
         file.write(br)
         file.write(br)
-        file.write('Среднее арифметическое потраченного {}р.'.format(average))
+        file.write('An arithmetic average of spent: {}'.format(average))
         file.write(br)
         file.write(br)
-        file.write('Должны скинуть: ')
+        file.write('Who must pay: ')
         file.write(br)
         for i in range(0, len(minus_people_rep)):
             debt = '{} - {}р.'.format(minus_people_rep[i], minus_sum_rep[i])
             file.write(debt)
             file.write(br)
         file.write(br)
-        file.write('Должны получить: ')
+        file.write('Who must be payed: ')
         file.write(br)
         for i in range(0, len(plus_people_rep)):
             overpay = '{} - {}р.'.format(plus_people_rep[i], plus_sum_rep[i])
             file.write(overpay)
             file.write(br)
         file.write(br)
-        file.write('Список транзакций: ')
+        file.write('Transaction list: ')
         file.write(br)
         for i in range(0, len(transaction_minus)):
             transactions = '{} ---> {}  {}р.'.format(
