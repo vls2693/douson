b35b0c34ddc1441d4fd3f1118e3428ff689a1729
VLS <belyakov3@gmail.com>
2021-03-03 22:52:14 +0300

    добавил скрипт (пока сырой) для добавления новых полей по нажатию кнопки

---
+++ flask/static/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('it works!')
+})
+
+var app = document.querySelector('.form')
+var title = document.createElement('fieldset')
+title.innerHTML = '<input class=\"name\" name=\"name2\" placeholder=\"Name\" type=\"text\">\n                <input class=\"sum\" name=\"sum2\" placeholder=\"Sum\" type=\"number\">'
+
+
+app.appendChild(title)
+
+console.log('title', title)
