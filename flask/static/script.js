38232edc6a57b921987e39066cf7c489cd31c25a
VLS <belyakov3@gmail.com>
2021-03-05 21:42:26 +0300

    изменил вёрстку, теперь поля с именем и суммой добавляются после других полей, а не под кнопкой "Отправить"

--- flask/static/script.js
+++ flask/static/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('it works!')
 })
 
-var app = document.querySelector('.form')
+var app = document.querySelector('.bunch')
 var title = document.createElement('fieldset')
 title.innerHTML = '<input class=\"name\" name=\"name2\" placeholder=\"Name\" type=\"text\">\n                <input class=\"sum\" name=\"sum2\" placeholder=\"Sum\" type=\"number\">'
 
