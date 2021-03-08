d290087749bbfd2bcefcbd728bb3a95cb712cbbe
VLS <belyakov3@gmail.com>
2021-03-08 22:55:53 +0300

    добавил возможность удаления полей

--- flask/static/script.js
+++ flask/static/script.js
@@ -6,7 +6,14 @@
 
 function addString() {
     var app = document.querySelector('.bunch')
-    var title = document.createElement('fields')
+    var title = document.createElement('fieldset')
     title.innerHTML = '<input class=\"name\" name=\"name\" required placeholder=\"Name\" type=\"text\">\n                <input class=\"sum\" name=\"sum\" required placeholder=\"Sum\" type=\"number\">'
     app.appendChild(title)
+}
+
+function removeString() {
+    var parent = document.querySelector('.bunch')
+    var elem = document.querySelector('fieldset')
+    parent.removeChild(elem)
+//    elem.remove()
 }
